1b791f2b6953ac85264e96dd6a0a154ec4c0a9a3
Aleksander13457 <aleksander13457@gmail.com>
2019-02-10 17:45:26 +0100

    Operations on variables

---
+++ src/OperationsOnVariables.java
@@ -0,0 +1,10 @@
+public class OperationsOnVariables {
+    public static void main(String[] args) {
+        int number = 21;
+        if (number % 7 == 0) {
+            System.out.println("The number 7 is the divisor of the number " + number);
+        } else {
+            System.out.println("The number 7 is not a divisor of the number " + number);
+        }
+    }
+}
